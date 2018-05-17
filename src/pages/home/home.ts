eea476d94791ed8fcc8bf476f459f93f3e2251ba
teddy <teddy.coevoet.pro@outlook.fr>
2018-05-17 16:23:43 +0200

    changement de page avec retour

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToAbout() {
+    // ici on change lapage d'accueil par la page à propos au clic sur le button
+    this.navCtrl.push(AboutPage);
   }
 
 }
